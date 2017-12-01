8ee06e1154424a0f7099abc9b840751451a4f456
chenmingliang <229473390@qq.com>
2017-12-01 17:04:58 +0800

    增加JavaInterviewQuestion

--- app/src/main/java/com/cml/cmlinterview/MainActivity.java
+++ app/src/main/java/com/cml/cmlinterview/MainActivity.java
@@ -2,6 +2,7 @@ package com.cml.cmlinterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ShareData.main(new String[5]);
+
+        Integer integer = new Integer(5);
+        Integer integer2 = 5;
+
+
+        if(integer==integer2)
+            Log.e(tag,"integer==integer2:true");
+        else
+            Log.e(tag,"integer==integer2:false");
+
+//        if(integer==5)
+//            Log.e(tag,"integer==5:true");
+//        else
+//            Log.e(tag,"integer==5:false");
+//
+//        if(integer==128)
+//            Log.e(tag,"integer==128:true");
+//        else
+//            Log.e(tag,"integer==128:false");
+
     }
+
 
 }
